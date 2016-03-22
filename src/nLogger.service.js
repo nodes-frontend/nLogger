2957482d083982764dfd767eeece8e225f6f98cc
Alexander Hafstad <alexander@hafstad.dk>
2016-03-22 12:49:49 +0100

    test(nLoggerConfig): Added missing tests for nLoggerConfig and nLogger

--- src/nLogger.service.js
+++ src/nLogger.service.js
@@ -37,52 +37,52 @@
 			Public Methods
 		 */
 
-		function log(message, data, options) {
+		function log(message, data) {
 
 			var css = 'font-weight: bold;';
 
 			if(defaults.console.enable) {
-				console.log('%c ' + message, css , (data || ''));
+				console.log('%c ' + message, css, (data || ''));
 			}
 
 		}
 
-		function error(message, data, options) {
+		function error(message, data) {
 
 			var css = 'font-weight: bold; color: red;';
 
 			if(defaults.console.enable) {
-				console.error('%c ' + message, css , (data || ''));
+				console.error('%c ' + message, css, (data || ''));
 			}
 
 		}
 
-		function warning(message, data, options) {
+		function warning(message, data) {
 
 			var css = 'font-weight: bold; color: #de770f;';
 
 			if(defaults.console.enable) {
-				console.warn('%c ' + message, css , (data || ''));
+				console.warn('%c ' + message, css, (data || ''));
 			}
 
 		}
 
-		function success(message, data, options) {
+		function success(message, data) {
 
 			var css = 'font-weight: bold; color: #3a945b;';
 
 			if(defaults.console.enable) {
-				console.log('%c ' + message, css , (data || ''));
+				console.log('%c ' + message, css, (data || ''));
 			}
 
 		}
 
-		function info(message, data, options) {
+		function info(message, data) {
 
 			var css = 'font-weight: bold; color: #74bfdd;';
 
 			if(defaults.console.enable) {
-				console.info('%c ' + message, css , (data || ''));
+				console.info('%c ' + message, css, (data || ''));
 			}
 
 		}
--- test/nLoggerServiceSpec.js
+++ test/nLoggerServiceSpec.js
@@ -1,30 +1,229 @@
-describe('nLoggerConfig', function() {
+var nLogger;
+var nLoggerConfig;
+var nLoggerConfigDefaults;
+var DEBUG_ENV;
+var nLoggerRequiredFunctions = ['log', 'error', 'warning', 'success', 'info'];
 
-	var nLoggerConfigProvider;
-	var nLoggerConfig;
+function _beforeEach() {
+	module('nCore.nLogger.factory');
 
-	beforeEach(function() {
-		module('nCore.nLogger.config');
+	module(function($provide) {
+		$provide.provider('nLoggerConfig', function() {
+
+			this.$get = function() {
+				return nLoggerConfigDefaults;
+			};
+		});
+
+		$provide.constant('DEBUG_ENV', DEBUG_ENV);
 	});
 
-	// What should the feature do?
-	it('should return defaults', function() {
+	inject(['nLogger', function(_nLogger) {
+		nLogger = _nLogger;
+	}]);
+}
 
-		inject(['nLoggerConfig', function(_nLoggerConfig) {
-			nLoggerConfig = _nLoggerConfig; // to use the instance in other parts
-		}]);
+describe('nLogger - general', function() {
 
-		// What is the actual output?
-		var actual = nLoggerConfig;
+	beforeEach(function() {
 
-		// What is the expected output?
-		var expected = {
+		DEBUG_ENV 					= true;
+		nLoggerConfigDefaults 		= {
 			blacklist: ['log', 'warn', 'info', 'error'],
 			console: {
 				enable: true
 			}
 		};
 
-		expect(actual).toEqual(expected);
+		_beforeEach();
+	});
+
+	nLoggerRequiredFunctions.forEach(function(expected){
+
+		// What should the feature do?
+		it('should have "' + expected + '" defined', function() {
+
+			// What is the actual output?
+			var actual = nLogger[expected];
+
+			// What is the expected output?
+			expect(actual).toEqual(jasmine.any(Function));
+		});
+	});
+});
+
+describe('nLogger - Production', function() {
+	// TODO: TEST IF CONSOLE[TYPE] is BEING OVERWRITTEN DEPENDING ON DEBUG_ENV
+});
+
+describe('nLogger - Development', function() {
+	// TODO: TEST IF CONSOLE[TYPE] is BEING OVERWRITTEN DEPENDING ON DEBUG_ENV
+});
+
+describe('nLogger - console enabled', function() {
+
+	beforeEach(function() {
+
+		nLoggerConfigDefaults = {
+			console: {
+				enable: true
+			}
+		};
+
+		_beforeEach();
+	});
+
+	// What should the feature do?
+	it('"log" should call console.log', function() {
+
+		spyOn(console, 'log');
+
+		nLogger.log();
+
+		// What is the actual output?
+		var actual = console.log;
+
+		// What is the expected output?
+		expect(actual).toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"error" should call console.error', function() {
+
+		spyOn(console, 'error');
+
+		nLogger.error();
+
+		// What is the actual output?
+		var actual = console.error;
+
+		// What is the expected output?
+		expect(actual).toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"warning" should call console.warn', function() {
+
+		spyOn(console, 'warn');
+
+		nLogger.warning();
+
+		// What is the actual output?
+		var actual = console.warn;
+
+		// What is the expected output?
+		expect(actual).toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"success" should call console.log', function() {
+
+		spyOn(console, 'log');
+
+		nLogger.success();
+
+		// What is the actual output?
+		var actual = console.log;
+
+		// What is the expected output?
+		expect(actual).toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"info" should call console.info', function() {
+
+		spyOn(console, 'info');
+
+		nLogger.info();
+
+		// What is the actual output?
+		var actual = console.info;
+
+		// What is the expected output?
+		expect(actual).toHaveBeenCalled();
+	});
+});
+
+describe('nLogger - console disabled', function() {
+
+	beforeEach(function() {
+
+		nLoggerConfigDefaults = {
+			console: {
+				enable: false
+			}
+		};
+
+		_beforeEach();
+	});
+
+	// What should the feature do?
+	it('"log" should not call console.log', function() {
+
+		spyOn(console, 'log');
+
+		nLogger.log();
+
+		// What is the actual output?
+		var actual = console.log;
+
+		// What is the expected output?
+		expect(actual).not.toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"error" should not call console.error', function() {
+
+		spyOn(console, 'error');
+
+		nLogger.error();
+
+		// What is the actual output?
+		var actual = console.error;
+
+		// What is the expected output?
+		expect(actual).not.toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"warning" should not call console.warn', function() {
+
+		spyOn(console, 'warn');
+
+		nLogger.warning();
+
+		// What is the actual output?
+		var actual = console.warn;
+
+		// What is the expected output?
+		expect(actual).not.toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"success" should not call console.log', function() {
+
+		spyOn(console, 'log');
+
+		nLogger.success();
+
+		// What is the actual output?
+		var actual = console.log;
+
+		// What is the expected output?
+		expect(actual).not.toHaveBeenCalled();
+	});
+
+	// What should the feature do?
+	it('"info" should not call console.info', function() {
+
+		spyOn(console, 'info');
+
+		nLogger.info();
+
+		// What is the actual output?
+		var actual = console.info;
+
+		// What is the expected output?
+		expect(actual).not.toHaveBeenCalled();
 	});
 });
